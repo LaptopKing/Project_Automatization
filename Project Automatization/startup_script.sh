c56534a09fdaeddc68020e7a83dccff6d2014455
LaptopKing <csordaspeter2002@gmail.com>
2020-11-11 21:03:55 +0100

    modified:   startup_script.sh

--- Project Automatization/startup_script.sh
+++ Project Automatization/startup_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -q 
+url = "https://raw.githubusercontent.com/LaptopKing/Project_Automatization/main/Project%20Automatization/startup_script.py"
+wget -q $url
 
 var1=$(find startup_script.py)
 python3 "$var1"
