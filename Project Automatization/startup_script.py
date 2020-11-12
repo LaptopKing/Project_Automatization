a3644d80b81093f3f74b998f307aa112b9879fe6
LaptopKing <csordaspeter2002@gmail.com>
2020-11-12 15:13:57 +0100

    modified:   startup_script.py

--- Project Automatization/startup_script.py
+++ Project Automatization/startup_script.py
@@ -14,24 +14,16 @@ alias = "alias make_project='var1=$(locate Sajt.sh); cd" + "$var1" + "; ./Sajt.s
 for g in range(len(bash_array)):
     if (bash_array[g] != "# Custom aliases" and g >= minus_length):
         bash_array.append("\n# Custom aliases")
-        g = 0
-        minus_length2 = len(bash_array) - 1
-        if (bash_array[g] != alias and g >= minus_length2):
-            bash_array.append("\n" + alias)
-    else:
-        pass
 
     print(bash_array[g])
 
 new_string = ""
 for lines in bash_array:
-    new_string = lines
-print(new_string)
-print(alias)
-"""
+    new_string = liness
+new_string = new_string + "\n" + alias
+
 bash_new = open(os.path.expanduser("~/.bashrc"), "w")
 bash_new.write(new_string)
 bash_new.flush()
 bash_new.close()
 os.system("clear")
-"""
