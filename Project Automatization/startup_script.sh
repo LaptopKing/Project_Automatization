1053e2e3aceb7aaf9156103651c1e1b27fdff98f
LaptopKing <csordaspeter2002@gmail.com>
2020-11-12 13:11:29 +0100

    modified:   startup_script.sh

--- Project Automatization/startup_script.sh
+++ Project Automatization/startup_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+sudo apt install python3
+sudo apt install python3.7
 var2="https://raw.githubusercontent.com/LaptopKing/Project_Automatization/main/Project%20Automatization/startup_script.py"
-wget "$var2"
-
-var1=$(find startup_script.py)
-python3 "$var1"
+wget -O - "$var2" | python3
