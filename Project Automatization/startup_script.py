3a7d79712f4820d818283b4238d16fe1553598b6
LaptopKing <csordaspeter2002@gmail.com>
2020-11-12 20:55:09 +0100

    modified:   startup_script.py

--- Project Automatization/startup_script.py
+++ Project Automatization/startup_script.py
@@ -10,12 +10,15 @@ alias = "alias make_project='var1=$(locate Sajt.sh);" + '"$var1"'
 bash_new = open(os.path.expanduser("~/.bashrc"), "a+")
 
 new_string = "\n# Custom aliases" + "\n" + alias
+is_or_not = False
 for lines in bash_array:
     if (lines == "# Custom aliases" or lines == alias):
-        pass
+        is_or_not = False
     else:
-        bash_new.write(new_string)
-        break
+        is_or_not = True
+
+if (is_or_not == True):
+    bash_new.write(new_string)
 
 bash_new.flush()
 bash_new.close()
